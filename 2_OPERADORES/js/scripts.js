b8375e1120a1408f9dd31e3a236f5eee1dd577e0
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-02-05 19:15:39 -0300

    começando a parte de JavaScript

---
+++ 2_OPERADORES/js/scripts.js
@@ -0,0 +1,11 @@
+// 1 - Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+// 2 - Operações Aritméticas
+console.log(5 + 2);
+console.log(6 - 5);
+console.log(15 * 2);
+console.log(10 / 2)
+console.log(5 + (4 * 2));
