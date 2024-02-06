0166eff3405264ccf6d2911ee5cc71a5cef22e44
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-02-06 18:44:52 -0300

    novos conhecimentos

--- 2_OPERADORES/js/scripts.js
+++ 2_OPERADORES/js/scripts.js
@@ -9,3 +9,21 @@ console.log(6 - 5);
 console.log(15 * 2);
 console.log(10 / 2)
 console.log(5 + (4 * 2));
+
+// 3 - Special Numbers
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
+console.log(2 * 'love');
+
+// 4 - Strings
+console.log("Um texto");
+console.log('Outro texto');
+console.log(`Mais um texto`);
+
+console.log(typeof "Um texto");
+console.log(typeof 'Outro texto');
+
+// 5 - Símbolos Especiais em String
+console.log("Testando a \n quebra de linha");
+console.log("Espaçamento \t de tab");
