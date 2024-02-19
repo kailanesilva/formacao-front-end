09f8f40d3e0a74c4355bd1e48670629c1dd379ff
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-02-19 19:14:41 -0300

    Nova seção iniciada

---
+++ 3_ESTRUTURAS/js/scripts.js
@@ -0,0 +1,17 @@
+// 1 - Variáveis
+let nome = "Kailane";
+
+console.log(nome);
+
+nome = "Kailane Silva";
+
+console.log(nome);
+
+const idade = 19;
+
+console.log(idade);
+
+//variáveis como const não podem ter seus valores mudados. Uma vez atríbuido, não aceita mudanças.
+
+console.log(typeof nome);
+console.log(typeof idade);
