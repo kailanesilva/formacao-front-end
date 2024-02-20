55e8e83e460439efc65c7b95a49c74c8a942a5db
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-02-20 18:51:24 -0300

    novos conteúdos

--- 3_ESTRUTURAS/js/scripts.js
+++ 3_ESTRUTURAS/js/scripts.js
@@ -14,4 +14,33 @@ console.log(idade);
 //variáveis como const não podem ter seus valores mudados. Uma vez atríbuido, não aceita mudanças.
 
 console.log(typeof nome);
+
 console.log(typeof idade);
+
+// 2 - Mais Sobre Variáveis
+//let 2teste = "inválido"
+//let @teste = "inválido"
+
+let a = 10, b = 20, c = 30;
+
+console.log(a, b, c);
+
+let nomecompleto = "Kailane Silva";
+
+let nomeCompleto = "João da Silva";
+
+console.log(nomecompleto);
+
+console.log(nomeCompleto);
+
+let _teste = "ok";
+let $teste = "ok";
+
+console.log(_teste, $teste);
+
+// 3 - Nomes reservados - Jamais declarar variáveis com funcionalidades do JavaScript, como:
+/* break case catch class const continue debbuger default delete do else enum export extends
+false finally function if implements import interface in instanceof let new package
+private protected public return static super switch this throw true try typeof
+var void while with yield*
+É possível unir ela a outra palavra pra formar a variável. Ex: let breakTeste = 1;*/
