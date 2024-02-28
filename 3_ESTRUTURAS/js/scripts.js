1e4933a92328e18afd2ee3445fbd040300afadb1
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-02-28 11:08:37 -0300

    exercícios novos!

--- 3_ESTRUTURAS/js/scripts.js
+++ 3_ESTRUTURAS/js/scripts.js
@@ -46,13 +46,48 @@ var void while with yield*
 É possível unir ela a outra palavra pra formar a variável. Ex: let breakTeste = 1;*/
 
 // 3 - Prompt
-const age = prompt("Digite a sua idade:");
+//const age = prompt("Digite a sua idade:");
 
-console.log(`Você tem ${age} anos.`);
+//console.log(`Você tem ${age} anos.`);
 
 // 4 - Alert
-alert("Testando a função alert");
+//alert("Testando a função alert");
 
-const z = 10;
+//const z = 10;
 
-alert(`O número é ${z}`);
+//alert(`O número é ${z}`);
+
+// 5 - Math
+console.log(Math.max(5, 2, 1, 10));
+
+console.log(Math.min(1, 2, 3, 4, 5));
+
+console.log(Math.floor(5.14));
+
+console.log(Math.ceil(5.14));
+
+// 6 - Console
+console.log("teste!");
+
+console.error("Erro!");
+
+console.warn("Aviso!");
+
+// 7 - If
+const m = 10;
+
+if(m > 5){
+    console.log("M é maior que 5!");
+}
+
+const user = "João";
+
+if(user === "João"){
+    console.log("Olá, João!");
+}
+
+if(user === "Maria"){
+    console.log("Olá, Maria!");
+}
+
+console.log(user === "João", user === "Maria");
