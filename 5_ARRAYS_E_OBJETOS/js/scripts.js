dbf587c04657e0fba8ca353cea96290c7e8d23bc
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-04-05 15:48:05 -0300

    propriedades novas

--- 5_ARRAYS_E_OBJETOS/js/scripts.js
+++ 5_ARRAYS_E_OBJETOS/js/scripts.js
@@ -8,3 +8,38 @@ console.log(typeof lista);
 const itens = ["Kailane", true, 2, 4.12, []];
 
 console.log(itens);
+
+// 2 - Mais Sobre Arrays
+const arr = ["a","b","c","d","e"];
+
+console.log(arr[0]);
+
+console.log(arr[2]);
+
+console.log(arr[83]);
+
+// 3 - Propriedades
+const numbers = [5, 3, 4];
+
+console.log(numbers.length);
+
+console.log(numbers['length']);
+
+const myName = "Kailane";
+
+console.log(myName.length);
+
+// 4 - Métodos
+const otherNumbers = [1, 2, 3];
+
+const allNumbers = numbers.concat(otherNumbers);
+
+console.log(allNumbers);
+
+const text = "algum texto";
+
+console.log(text.toUpperCase());
+
+console.log(typeof text.toUpperCase);
+
+console.log(text.indexOf("g"));
