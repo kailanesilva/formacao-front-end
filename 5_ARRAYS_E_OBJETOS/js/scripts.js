de5de4630b2a16039b03db6a0516af8414e9b111
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-05-09 11:43:28 -0300

    conteúdo novo

--- 5_ARRAYS_E_OBJETOS/js/scripts.js
+++ 5_ARRAYS_E_OBJETOS/js/scripts.js
@@ -191,3 +191,24 @@ console.log(myElements.lastIndexOf("Abacate"));
 console.log(myElements.indexOf("Mamão"));
 
 console.log(myElements.lastIndexOf("Mamão"));
+
+// 14 - Slice
+const testeSlice = ["a", "b", "c", "d", "e", "f"];
+
+const subArray = testeSlice.slice(2, 4);
+
+console.log(subArray);
+
+console.log(testeSlice);
+
+const subArray2 = testeSlice.slice(2, 4 + 1);
+
+console.log(subArray2);
+
+const subArray3 = testeSlice.slice(10, 20);
+
+console.log(subArray3);
+
+const subArray4 = testeSlice.slice(2);
+
+console.log(subArray4);
