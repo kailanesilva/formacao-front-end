105c05643e4397891a3bfd85f6dcf90b79f24b66
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-06-11 19:43:16 -0300

    conteúdo novo

--- 6_OOP/js/scripts.js
+++ 6_OOP/js/scripts.js
@@ -12,13 +12,53 @@ animal.latir();
 
 // 2 - Aprofundando em Métodos
 const pessoa = {
-    nome: "kailane",
+    nome: "Kailane",
 
     getNome: function() {
         return this.nome;
+    },
+
+    setNome: function(novoNome) {
+        this.nome = novoNome;
     },
 };
 
 console.log(pessoa.nome);
 
-console.log(pessoa.getNome);
+console.log(pessoa.getNome());
+
+pessoa.setNome("Joaquim");
+
+console.log(pessoa.getNome());
+
+// 3 - Prototype
+const text = "asd";
+
+console.log(Object.getPrototypeOf(text));
+
+const bool = true;
+
+console.log(Object.getPrototypeOf(bool));
+
+const arr = [];
+
+console.log(arr.length); // propriedade herdada pelo objeto pai
+
+console.log(Object.getPrototypeOf(arr));
+
+// 4 - Mais Sobre Prototype
+const myObject = { 
+    a: "b"
+};
+
+console.log(Object.getPrototypeOf(myObject));
+
+console.log(Object.getPrototypeOf(myObject) === Object.prototype);
+
+const mySecondObject = Object.create(myObject);
+
+console.log(mySecondObject);
+
+console.log(mySecondObject.a);
+
+console.log(Object.getPrototypeOf(mySecondObject) === myObject);
