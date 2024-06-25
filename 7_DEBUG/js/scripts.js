f330c8de6fc34aac996e9be5a893721c075d366a
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-06-25 11:50:05 -0300

    conteúdo novo

---
+++ 7_DEBUG/js/scripts.js
@@ -0,0 +1,31 @@
+// 1 - Strict
+"use strict";
+
+//opa = "Teste";
+
+const opa = "Teste";
+
+//const undefined = 10;
+
+//delete[].length;
+
+// 2 - Console.log
+let a = 1;
+let b = 2;
+
+if(a == 1) {
+    a = b + 2
+}
+
+console.log(a);
+
+for(let i = 0; i < b; i++){
+    a = a + 2
+    console.log(a);
+}
+
+if(a > 5) {
+    a = 25
+}
+
+console.log(a);
