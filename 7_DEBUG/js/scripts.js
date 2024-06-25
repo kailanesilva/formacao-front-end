0e74cdf699b205acdcdeff9fd109a778e902edcc
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-06-25 15:42:59 -0300

    conteúdo novo

--- 7_DEBUG/js/scripts.js
+++ 7_DEBUG/js/scripts.js
@@ -29,3 +29,53 @@ if(a > 5) {
 }
 
 console.log(a);
+
+// 3 - Debugger
+let c = 1;
+let d = 2;
+
+if(c == 1) {
+    c = d + 2
+}
+
+//debugger;
+
+for(let i = 0; i < d; i++) {
+    c = c + 2
+}
+
+console.log("Executou o loop");
+
+//debugger; 
+
+
+if(c > 5) {
+    c = 25
+}
+
+// 4 - Tratamento de Dados
+function checkNumber(n) {
+    const result = Number(n)
+
+    if(Number.isNaN(result)) {
+        console.log("Valor incorreto!");
+        return;
+    }
+
+    console.log("Valor correto!");
+    return result;
+}
+
+checkNumber(5);
+checkNumber("10");
+checkNumber({});
+checkNumber("teste");
+
+// 5 - Exceptions
+let x = 10;
+
+if(x != 11) {
+    throw new Error("O valor de x não pode ser diferente de 11!")
+}
+
+console.log(x);
