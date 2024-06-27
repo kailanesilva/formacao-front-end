3170c4f390a481f3dc2e8fc081ea94cc6aae6bd9
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-06-27 14:31:59 -0300

    conteúdo novo

---
+++ 8_ASYNC/js/scripts.js
@@ -0,0 +1,19 @@
+// 1 - setTimeout
+console.log("Ainda não executou");
+
+setTimeout(function() {
+
+    console.log("Requisição assíncrona");
+
+}, 2000);
+
+console.log("Ainda não executou 2");
+
+// 2 - setInterval
+console.log("Ainda não começou");
+
+//setInterval(function() {
+    //console.log("Intervalo assíncrono");
+//}, 2000);
+
+console.log("Ainda não começou 2");
