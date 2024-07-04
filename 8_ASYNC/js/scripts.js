2444eaf7cb5b792c37016a29bd630b945bfd773e
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-07-04 18:37:03 -0300

    conteúdos novos

--- 8_ASYNC/js/scripts.js
+++ 8_ASYNC/js/scripts.js
@@ -63,3 +63,65 @@ console.log(a, b);
 a.then((v) => console.log (`O resultado é ${v}`)).catch((err) => console.log(`Um erro ocorreu: ${err}`));
 
 b.then((v) => console.log (`O resultado é ${v}`)).catch((err) => console.log(`Um erro ocorreu: ${err}`));
+
+// 6 - Resolvendo Várias Promises
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(function() {
+        resolve(10);
+    }, 3000)
+})
+
+const p2 = Promise.resolve(10 + 10);
+
+const p3 = new Promise((resolve, reject) => {
+    if(30 > 10){
+        resolve(30);
+    } else {
+        reject("Erro!");
+    }
+})
+
+Promise.all([p1, p2, p3]).then((values) => console.log(values));
+
+// 7 - Async Functions
+async function somarComDelay(a, b) {
+    return a + b;
+}
+
+somarComDelay(2, 4).then((value) => {
+    console.log(`O valor da soma é ${value}`);
+})
+
+console.log("Teste async");
+
+// 8 - Async Await
+function resolveComDelay() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Resolveu a Promise");
+        }, 2000)
+    })
+}
+
+async function chamadaAsync() {
+    console.log("Chamando a Promise e esperando o resultado");
+    const result = await resolveComDelay()
+    console.log(`O resultado chegou: ${result}`);
+}
+
+chamadaAsync();
+
+// 9 - Generators
+function* generator() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const gen = generator();
+
+console.log(gen.next().value);
+
+console.log(gen.next().value);
+
+console.log(gen.next().value);
