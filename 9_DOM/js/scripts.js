725aeb9996e444b37cd1ed82f547bd8c54ba153f
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-07-10 17:09:32 -0300

    conteúdo novo

--- 9_DOM/js/scripts.js
+++ 9_DOM/js/scripts.js
@@ -39,3 +39,42 @@ const header = title.parentElement;
 console.log(header);
 
 header.insertBefore(p, title);
+
+// 7 - AppendChild
+const navLinks = document.querySelector("nav ul");
+
+const li = document.createElement("li");
+
+navLinks.appendChild(li);
+
+// 8 - ReplaceChild
+const h2 = document.createElement("h2");
+
+h2.textContent = "Meu Novo Título!";
+
+header.replaceChild(h2, title);
+
+// 9 - CreateTextNode
+
+const myText = document.createTextNode("Agora vamos colocar mais um título");
+
+console.log(myText);
+
+const h3 = document.createElement("h3");
+
+h3.appendChild(myText);
+
+console.log(h3);
+
+mainContainer.appendChild(h3);
+
+// 10 - Trabalhando Com Atributos
+const firstLink = navLinks.querySelector("a");
+
+console.log(firstLink);
+
+firstLink.setAttribute("href", "https://google.com");
+
+console.log(firstLink.getAttribute("href"));
+
+firstLink.setAttribute("target", "_blank");
