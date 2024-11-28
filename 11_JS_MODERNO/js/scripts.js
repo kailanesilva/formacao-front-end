3957374ad48b268582c31d752c4b3c766b971fa1
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-11-28 11:28:16 -0300

    conteúdo novo

---
+++ 11_JS_MODERNO/js/scripts.js
@@ -0,0 +1,98 @@
+// 1 - Var, let e const
+var x = 10;
+var y = 15;
+
+if(y > 10){
+    var x = 5;
+    console.log(x);
+}
+
+console.log(x);
+
+let a = 10;
+let b = 15;
+
+if(b > 10){
+    let a = 5;
+    console.log(a);
+}
+
+console.log(a);
+
+function logName(){
+    const name = "Pedro";
+    console.log(name);
+}
+
+const name = "Matheus";
+
+logName();
+
+console.log(name);
+
+// 2 - Arrow function
+const sum = function(a, b){
+    return a + b;
+}
+
+const arrowSum = (a, b) => a + b;
+
+console.log(sum(5, 5));
+
+console.log(arrowSum(5, 5));
+
+const greeting = (name) => {
+    if(name){
+        return `Hello ${name}!`;
+    } else{
+        return "Hello!";
+    }
+}
+
+console.log(greeting());
+
+console.log(greeting("Kailane"));
+
+const user = {
+    name: "Théo",
+    sayUserName(){
+        setTimeout(function() {
+
+            console.log(this);
+            console.log(`Username: ${this.name}`);
+
+        }, 1000)
+    },
+    sayUserNameArrow(){
+        setTimeout(() => {
+            console.log(this);
+            console.log(`Username: ${this.name}`);
+        }, 2000)
+    }
+}
+
+// user.sayUserName();
+// user.sayUserNameArrow();
+
+// 3 - Filter
+const arr = [1, 2, 3, 4, 5, 6];
+
+const highNumbers = arr.filter((n) => {
+    if(n >= 3){
+        return n;
+    }
+})
+
+console.log(highNumbers);
+
+const users = [
+    {name: "Matheus", available: true},
+    {name: "João", available: false},
+    {name: "Maria", available: true},
+    {name: "Josias", available: false},
+    {name: "Kleber", available: true}
+]
+
+const availableUsers = users.filter((user) => user.available);
+
+console.log(availableUsers);
