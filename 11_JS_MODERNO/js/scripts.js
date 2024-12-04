2ead4b075ac0696cfeacc5fa4da2cb72947c5d20
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2024-12-04 18:50:03 -0300

    conteúdo concluído

--- 11_JS_MODERNO/js/scripts.js
+++ 11_JS_MODERNO/js/scripts.js
@@ -136,3 +136,66 @@ const productDetails = {
 const {name: productName, price, category, color} = productDetails;
 
 console.log(`O nome do produto é ${productName}, ele custa R$${price} e é da cor ${color}`);
+
+// 7 - Spread Operator
+const a1 = [1, 2, 3];
+const a2 = [4, 5, 6];
+
+const a3 = [...a1, ...a2];
+
+console.log(a3);
+
+const a4 = [0, ...a3, 7];
+
+console.log(a4);
+
+const carName = {name: "Gol"};
+const carBrand = {brand: "VW"};
+const otherInfos = {km: 1000000, price: 49999};
+
+const car = {...carName, ...carBrand, ...otherInfos};
+
+console.log(car);
+
+// 8 - Classes
+class Product {
+    constructor(name, price){
+    this.name = name
+    this.price = price
+    }
+
+    productWithDiscount(discount){
+        return this.price * ((100 - discount) / 100)
+    }
+}
+
+const shirt = new Product("Camisa gola V", 20);
+
+console.log(shirt);
+
+console.log(shirt.productWithDiscount(10));
+console.log(shirt.productWithDiscount(15));
+
+// 9 - Herança
+class ProductWiththAttributes extends Product {
+
+    constructor(name, price, colors) {
+        super(name, price)
+        this.colors = colors
+    }
+
+    showColors(){
+        console.log("As cores são:");
+        this.colors.forEach((color) => {
+            console.log(color);
+        })
+    }
+}
+
+const hat = new ProductWiththAttributes("Chapéu", 29.99, ["Preto", "Amarelo", "Verde"]);
+
+console.log(hat);
+
+console.log(hat.name);
+
+hat.showColors();
