a6452ffc4acd47d680bc521b0e29ebdfbc328c8f
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2025-01-08 11:03:42 -0300

    conteúdo novo

--- 12_AXIOS/js/scripts.js
+++ 12_AXIOS/js/scripts.js
@@ -5,7 +5,15 @@ console.log(axios);
 const getData = async() => {
     try {
 
-        const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users",
+            // 4 - Definindo headers
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    custom: "header",
+                },
+            }
+        )
 
         console.log(response);
 
@@ -17,3 +25,32 @@ const getData = async() => {
 }
 
 getData();
+
+// 3 - Imprimindo dados na tela
+const container = document.querySelector("#user-container");
+
+const printData = async() => {
+    const data = await getData();
+
+    console.log(data);
+
+    data.forEach((user) => {
+        const div = document.createElement("div");
+
+        const nameElement = document.createElement("h2");
+
+        nameElement.textContent = user.name;
+
+        div.appendChild(nameElement);
+
+        const emailElement = document.createElement("p");
+
+        emailElement.textContent = user.email;
+
+        div.appendChild(emailElement);
+
+        container.appendChild(div);
+    })
+}
+
+printData();
